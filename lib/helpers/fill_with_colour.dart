710ca6a5949b959291e7f0b0fd48d589a0a5dc88
Topi Salonen <topi@identio.fi>
2024-08-14 08:29:38 +0300

    Refactor fill logic to fix issue with undo

--- lib/helpers/fill_with_colour.dart
+++ lib/helpers/fill_with_colour.dart
@@ -1,29 +1,51 @@
 import 'dart:ui';
 
-bool _pixelIsValid(int size, bool row, int i1, int i2, Color color1, List<Color> pixelColours) {
-  if (i2 > (size * size - 1) || i2 < 0) {
-    // Index out of bound
-    return false;
-  }
-
-  if (row && (i1 / size).floor() != (i2 / size).floor()) {
-    // Different row, don't fill
-    return false;
-  }
-
-  if (color1 != pixelColours[i2]) {
-    // Different colour, don't fill
-    return false;
-  }
-
-  return true;
+bool _isInBounds(int i1, int i2, int size) {
+  return i2 < size * size && i2 >= 0;
 }
 
-List<Color> fillWithColour(Color colour, List<Color> pixelColours, int gridSize, int pi, Color initialColor) {
+bool _isHorizontallyAdjacent(int i1, int i2, int size) {
+  return _isInBounds(i1, i2, size) && (i1 / size).floor() == (i2 / size).floor();
+}
+
+bool _isFillable(Color color1, Color color2) {
+  return color1 == color2;
+}
+
+List<int> _getFillableAdjacentPixels(int pi, int gridSize, initialColour, pixelColours) {
+  List<int> adjacentPixels = [];
+
+  final int iLeft = pi - 1;
+  if (_isHorizontallyAdjacent(pi, iLeft, gridSize) && _isFillable(initialColour, pixelColours[iLeft])) {
+    adjacentPixels.add(iLeft);
+  }
+
+  final int iRight = pi + 1;
+  if (_isHorizontallyAdjacent(pi, iRight, gridSize) && _isFillable(initialColour, pixelColours[iRight])) {
+    adjacentPixels.add(iRight);
+  }
+
+  final int iAbove = pi - gridSize;
+  if (_isInBounds(pi, iAbove, gridSize) && _isFillable(initialColour, pixelColours[iAbove])) {
+    adjacentPixels.add(iAbove);
+  }
+
+  final int iBelow = pi + gridSize;
+  if (_isInBounds(pi, iBelow, gridSize) && _isFillable(initialColour, pixelColours[iBelow])) {
+    adjacentPixels.add(iBelow);
+  }
+
+  return adjacentPixels;
+}
+
+List<Color> fillWithColour(Color fillColour, List<Color> pixelColours, int gridSize, int pi, [Color? initialColourProp]) {
   /*
-    To fill an area, we need to check if the pixels next to
-    the pixel that was pressed have the same color, and if so color them,
-    and repeat the process until no valid pixels are found
+    To fill an area, we need to check if the adjacent pixels ar
+      1) A valid index
+      2) Have the same colour
+
+    We then have to repeat the process on adjacent pixels of adjacent pixels
+    until no more valid pixels are found
 
     Example:
     □ □ ■ □ □       ■ ■ ■ □ □
@@ -32,46 +54,27 @@ List<Color> fillWithColour(Color colour, List<Color> pixelColours, int gridSize,
 
   */
 
-  if (colour == initialColor) {
+  final initialColour = initialColourProp ?? pixelColours[pi];
+
+  if (fillColour == initialColour) {
     // Already filled
     return pixelColours;
   }
 
-  // Color pressed pixel
-  pixelColours[pi] = colour;
+  var filledColours = [...pixelColours];
 
-  // The following logic could be cleaned up, but it works
-  // Color each adjacent pixel if it's valid
+  // Fill current pixel
+  filledColours[pi] = fillColour;
 
-  final int iLeft = pi - 1;
-  if (_pixelIsValid(gridSize, true, pi, iLeft, initialColor, pixelColours)) {
-    pixelColours[iLeft] = colour;
+  // Get valid adjacent pixels
+  final adjacentPixels = _getFillableAdjacentPixels(pi, gridSize, initialColour, pixelColours);
 
-    pixelColours = fillWithColour(colour, pixelColours, gridSize, iLeft, initialColor);
+  // Fill adjacent pixels
+  // Recursion for adjacent pixels of adjacent pixels
+  for (var ai in adjacentPixels) {
+    filledColours[ai] = fillColour;
+    filledColours = fillWithColour(fillColour, filledColours, gridSize, ai, initialColour);
   }
 
-  final int iRight = pi + 1;
-  if (_pixelIsValid(gridSize, true, pi, iRight, initialColor, pixelColours)) {
-    pixelColours[iRight] = colour;
-
-    pixelColours = fillWithColour(colour, pixelColours, gridSize, iRight, initialColor);
-  }
-
-  final int iAbove = pi - gridSize;
-  if (_pixelIsValid(gridSize, false, pi, iAbove, initialColor, pixelColours)) {
-    pixelColours[iAbove] = colour;
-
-    pixelColours = fillWithColour(colour, pixelColours, gridSize, iAbove, initialColor);
-  }
-
-  final int iBelow = pi + gridSize;
-  if (_pixelIsValid(gridSize, false, pi, iBelow, initialColor, pixelColours)) {
-    pixelColours[iBelow] = colour;
-
-    pixelColours = fillWithColour(colour, pixelColours, gridSize, iBelow, initialColor);
-  }
-
-  return List<Color>.generate(
-      gridSize * gridSize, (i) => pixelColours[i]
-  );
+  return filledColours;
 }
